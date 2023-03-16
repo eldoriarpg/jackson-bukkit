8452b061734ca6c67d3c6c568ff7530fb8ea6ce6
Yannick Lamprecht <yannicklamprecht@live.de>
2023-03-16 21:16:57 +0100

    test dummy

---
+++ src/test/java/de/eldoria/jacksonbukkit/deserializer/BlockVectorDeserializerTest.java
@@ -0,0 +1,12 @@
+package de.eldoria.jacksonbukkit.deserializer;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BlockVectorDeserializerTest {
+
+    @Test
+    void deserialize() {
+    }
+}
---
+++ src/test/java/de/eldoria/jacksonbukkit/serializer/ColorSerializerTest.java
@@ -0,0 +1,12 @@
+package de.eldoria.jacksonbukkit.serializer;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ColorSerializerTest {
+
+    @Test
+    void serialize() {
+    }
+}
