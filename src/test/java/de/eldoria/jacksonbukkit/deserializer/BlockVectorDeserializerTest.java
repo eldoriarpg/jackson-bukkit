d8f5ca9b7032da553276f979a8279322259e58b3
Yannick Lamprecht <yannicklamprecht@live.de>
2023-03-16 21:59:15 +0100

    implement blockvector deserialization test

--- src/test/java/de/eldoria/jacksonbukkit/deserializer/BlockVectorDeserializerTest.java
+++ src/test/java/de/eldoria/jacksonbukkit/deserializer/BlockVectorDeserializerTest.java
@@ -1,12 +1,28 @@
 package de.eldoria.jacksonbukkit.deserializer;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import de.eldoria.jacksonbukkit.TestUtil;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static de.eldoria.jacksonbukkit.templates.BlockVectorTemplate.VECTOR;
+import static de.eldoria.jacksonbukkit.templates.BlockVectorTemplate.VECTORS;
 
-class BlockVectorDeserializerTest {
+class BlockVectorDeserializerTest implements TestUtil {
 
     @Test
-    void deserialize() {
+    void deserializeToJson() throws JsonProcessingException {
+        Assertions.assertEquals(json("vector"), toJson(VECTOR));
+        Assertions.assertEquals(json("vector_list"), toJson(VECTORS));
+    }
+    @Test
+    void deserializeToYaml() throws JsonProcessingException {
+        Assertions.assertEquals(yaml("vector"), toYaml(VECTOR));
+        Assertions.assertEquals(yaml("vector_list"), toYaml(VECTORS));
+    }
+    @Test
+    void deserializeToToml() throws JsonProcessingException {
+        Assertions.assertEquals(toml("vector"), toToml(VECTOR));
+        Assertions.assertEquals(toml("vector_list"), toToml(VECTORS));
     }
 }
---
+++ src/test/java/de/eldoria/jacksonbukkit/templates/BlockVectorTemplate.java
@@ -0,0 +1,13 @@
+package de.eldoria.jacksonbukkit.templates;
+
+import org.bukkit.util.BlockVector;
+
+import java.util.List;
+
+public class BlockVectorTemplate {
+    public static final BlockVector VECTOR = new BlockVector(1.549687, 58.52578, 8421257.577845);
+    public static final List<BlockVector> VECTORS = List.of(
+            new BlockVector(1.549687, 58.52578, 8421257.577845),
+            new BlockVector(1.549687, 58.52578, 8421257.577845),
+            new BlockVector(1.549687, 58.52578, 8421257.577845));
+}
