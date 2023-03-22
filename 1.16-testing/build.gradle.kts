79de35e1550bbb534cb3bc395199c3461f8c9268
Yannick Lamprecht <yannicklamprecht@live.de>
2023-03-22 23:02:37 +0100

    move configs to main test dependency, added legacy check for potion effect

---
+++ 1.16-testing/build.gradle.kts
@@ -0,0 +1,14 @@
+repositories {
+    maven("https://eldonexus.de/repository/codemc-nms/")
+}
+
+dependencies {
+    testImplementation(project(":core"))
+    implementation("com.destroystokyo.paper:paper-api:1.16.5-R0.1-20211218.081530-231")
+    testImplementation("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml")
+    testImplementation("com.fasterxml.jackson.dataformat:jackson-dataformat-toml")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.9.2")
+    testImplementation("org.assertj:assertj-core:3.24.2")
+    implementation(project(":testing"))
+    testImplementation("com.github.seeseemelk", "MockBukkit-v1.19", "2.29.0")
+}
