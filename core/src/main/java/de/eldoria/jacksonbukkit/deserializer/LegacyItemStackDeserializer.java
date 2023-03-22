e6bd6f98f8f16bafa4e6ed83483f46a466d80e40
Florian Fülling <46890129+RainbowDashLabs@users.noreply.github.com>
2023-03-23 00:00:39 +0100

    Properly implement legacy item stack serialization
    Fix bukkits stupid item serialisation

--- core/src/main/java/de/eldoria/jacksonbukkit/deserializer/LegacyItemStackDeserializer.java
+++ core/src/main/java/de/eldoria/jacksonbukkit/deserializer/LegacyItemStackDeserializer.java
@@ -9,7 +9,12 @@ import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.databind.DeserializationContext;
 import com.fasterxml.jackson.databind.JsonDeserializer;
 import com.fasterxml.jackson.databind.type.MapType;
+import de.eldoria.jacksonbukkit.entities.MapContainer;
+import org.bukkit.configuration.InvalidConfigurationException;
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.inventory.ItemStack;
+import org.yaml.snakeyaml.Yaml;
+import org.yaml.snakeyaml.constructor.Constructor;
 
 import java.io.IOException;
 import java.util.HashMap;
@@ -18,9 +23,20 @@ import java.util.HashMap;
  * Class for deserialization of {@link ItemStack} as a map.
  */
 public class LegacyItemStackDeserializer extends JsonDeserializer<ItemStack> {
+    private static final Yaml YAML = new Yaml(new Constructor(MapContainer.class));
+
     @Override
     public ItemStack deserialize(JsonParser p, DeserializationContext ctxt) throws IOException {
         MapType type = ctxt.getTypeFactory().constructMapType(HashMap.class, String.class, Object.class);
-        return ItemStack.deserialize(ctxt.readValue(p, type));
+
+        YamlConfiguration yamlConfiguration = new YamlConfiguration();
+        try {
+            yamlConfiguration.loadFromString(YAML.dump(new MapContainer(ctxt.readValue(p, type))));
+        } catch (InvalidConfigurationException e) {
+            throw new IOException(e);
+        }
+
+        return (ItemStack) yamlConfiguration.get("map");
     }
+
 }
--- core/src/main/java/de/eldoria/jacksonbukkit/serializer/LegacyItemStackSerializer.java
+++ core/src/main/java/de/eldoria/jacksonbukkit/serializer/LegacyItemStackSerializer.java
@@ -8,10 +8,11 @@ package de.eldoria.jacksonbukkit.serializer;
 import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.databind.JsonSerializer;
 import com.fasterxml.jackson.databind.SerializerProvider;
+import de.eldoria.jacksonbukkit.entities.MapContainer;
 import org.bukkit.configuration.file.YamlConfiguration;
-import org.bukkit.configuration.file.YamlRepresenter;
-import org.bukkit.configuration.serialization.ConfigurationSerialization;
 import org.bukkit.inventory.ItemStack;
+import org.yaml.snakeyaml.Yaml;
+import org.yaml.snakeyaml.constructor.Constructor;
 
 import java.io.IOException;
 
@@ -19,8 +20,13 @@ import java.io.IOException;
  * Class for serialization of {@link ItemStack}.
  */
 public class LegacyItemStackSerializer extends JsonSerializer<ItemStack> {
+    private static final Yaml YAML = new Yaml(new Constructor(MapContainer.class));
+
     @Override
     public void serialize(ItemStack value, JsonGenerator gen, SerializerProvider serializers) throws IOException {
-        gen.writeObject(value.serialize());
+        YamlConfiguration yamlConfiguration = new YamlConfiguration();
+        yamlConfiguration.set("map", value);
+        MapContainer items = YAML.load(yamlConfiguration.saveToString());
+        gen.writeObject(items.map);
     }
 }
