3e2d80e3176309531e53d83b375a61d99fcacfd5
Florian Fülling <46890129+RainbowDashLabs@users.noreply.github.com>
2023-03-22 23:12:46 +0100

    update buildfile

--- paper/build.gradle.kts
+++ paper/build.gradle.kts
@@ -1,5 +1,5 @@
 dependencies {
-    implementation(project(":core"))
+    api(project(":core"))
     compileOnly("io.papermc.paper", "paper-api", "1.19.4-R0.1-SNAPSHOT")
     testImplementation("io.papermc.paper", "paper-api", "1.19.4-R0.1-SNAPSHOT")
     testImplementation("com.github.seeseemelk", "MockBukkit-v1.19", "2.29.0")
