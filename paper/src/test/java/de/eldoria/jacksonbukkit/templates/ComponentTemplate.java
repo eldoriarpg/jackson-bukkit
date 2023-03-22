4f3a97b3abf55ee80274b4cc3c0dea036fb66f5f
Florian Fülling <46890129+RainbowDashLabs@users.noreply.github.com>
2023-03-22 16:45:43 +0100

    Apply spottless

--- paper/src/test/java/de/eldoria/jacksonbukkit/templates/ComponentTemplate.java
+++ paper/src/test/java/de/eldoria/jacksonbukkit/templates/ComponentTemplate.java
@@ -1,3 +1,8 @@
+/*
+ *     SPDX-License-Identifier: MIT
+ *
+ *     Copyright (C) EldoriaRPG Team and Contributor
+ */
 package de.eldoria.jacksonbukkit.templates;
 
 import net.kyori.adventure.text.Component;
