9bd3e8d100c48c7aebf2531eb8b0dd522d99ab6a
Florian Fülling <46890129+RainbowDashLabs@users.noreply.github.com>
2023-04-12 00:19:58 +0200

    Define type ids

--- core/src/main/java/de/eldoria/jacksonbukkit/entities/RecipeChoiceWrapper.java
+++ core/src/main/java/de/eldoria/jacksonbukkit/entities/RecipeChoiceWrapper.java
@@ -1,12 +1,17 @@
 package de.eldoria.jacksonbukkit.entities;
 
+import com.fasterxml.jackson.annotation.JsonSubTypes;
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import org.bukkit.inventory.RecipeChoice;
 
 /**
  * Class for wrapping a {@link RecipeChoice}.
  */
-@JsonTypeInfo(use = JsonTypeInfo.Id.CLASS, property = "@clazz")
+@JsonTypeInfo(use = JsonTypeInfo.Id.NAME, property = "@clazz")
+@JsonSubTypes({
+        @JsonSubTypes.Type(value = ShapedRecipeWrapper.class, name = "ShapedRecipe"),
+        @JsonSubTypes.Type(value = ShapelessRecipeWrapper.class, name = "ShapedRecipe")
+})
 public interface RecipeChoiceWrapper<T extends RecipeChoice> {
 
     /**
