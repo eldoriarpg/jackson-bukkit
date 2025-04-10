8c6457a2424c98ba87069b1e36784a966f0e7c6f
Yannick Lamprecht <1420893+yannicklamprecht@users.noreply.github.com>
2025-04-11 00:05:52 +0200

    update gradle and deactivate indra as it's f**d (#157)
    Update testing to work with newer Gradle versions

--- 1.16-testing/build.gradle.kts
+++ 1.16-testing/build.gradle.kts
@@ -5,10 +5,16 @@ repositories {
 dependencies {
     testImplementation(project(":core"))
     implementation("com.destroystokyo.paper:paper-api:1.16.5-R0.1-20211218.081530-231")
-    testImplementation("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml")
-    testImplementation("com.fasterxml.jackson.dataformat:jackson-dataformat-toml")
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.11.4")
+    testImplementation(libs.bundles.jackson)
+    testImplementation(testlibs.bundles.unittests)
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
     testImplementation("org.assertj:assertj-core:3.27.3")
     implementation(project(":testing"))
     testImplementation("com.github.seeseemelk", "MockBukkit-v1.19", "2.29.0")
+}
+
+tasks {
+    test {
+        useJUnitPlatform()
+    }
 }
